28e674e7d1350395e7daf8d5b0ca431ac60395f3
offlinehacker <jakahudoklin@gmail.com>
2011-10-25 10:44:27 +0200

    Add support for setting dump folder.

--- pipa_stream3/templates/stream_h264.py
+++ pipa_stream3/templates/stream_h264.py
@@ -7,13 +7,18 @@ class h264Tpl(pystache.View):
         self.values= values
     def dumpfile(self):
         date= strftime("%d-%m-%Y-%H-%M", gmtime())
-        if self.values.has_key("dumpf"):
-            return date+"_"+self.values["dumpf"]
+        if self.values.has_key("dumpfile"):
+            return date+"_"+self.values["dumpfile"]
         else:
             return date
+    def dumpfolder(self):
+        if self.values.has_key("dumpfolder"):
+            return self.values["dumpfolder]
+        else:
+            return "dump"
     def resolution(self):
         if self.values.has_key("res"):
             return self.values["res"]
         else:
-            return "640x480"
-            #return "768x567"
+            #return "640x480"
+            return "768x567"
