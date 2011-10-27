3562b73e605c07568f22e236b5a1ffd3f5a07344
offlinehacker <jakahudoklin@gmail.com>
2011-10-27 10:33:35 +0200

    Client and server with pid parameter support and changes to default paths

--- pipa_stream3/client_lirc.py
+++ pipa_stream3/client_lirc.py
@@ -18,10 +18,20 @@ def main():
                       help="Port where should i bind", dest="port")
     parser.add_option("--dump",
                       help="Folder where should i dump", dest="dump")
+    parser.add_option("--lirc-config",
+                      help="Path to lirc config", dest="lirc_config")
+    parser.add_option("--pid",
+                      help="Process pid", dest="pid")
     (options, args) = parser.parse_args()
 
     if options.daemon:
         ret= createDaemon()
+
+        if options.pid:
+            pid= os.getpid()
+            f= open(options.pid, "w")
+            f.write(str(pid))
+            f.close()
 
     if options.ip and options.port:
         ip= options.ip
@@ -34,7 +44,11 @@ def main():
     h264_client= None
 
     blocking= 1
-    if(pylirc.init("pylirc", "./conf", blocking)):
+    if options.lirc_config:
+        lirc_path= options.lirc_config
+    else:
+        lirc_path="/etc/pstream/conf"
+    if(pylirc.init("pylirc", lirc_path, blocking)):
         code= {"config": ""}
         while(code["config"] != "quit"):
             #Read next code
@@ -52,11 +66,13 @@ def main():
                 if (status!=StreamerStatus.RUNNING) or not status:
                     print("Creating new streamer")
                     h264= client.CreateStreamer("h264Stream")
-                    if options.dump:
-                        h264.SetStreamerValue("dumpfolder", options.dump)
                     print h264, client.GetStreamerInstances()
                     h264_client= xmlrpclib.ServerProxy("http://%s:%s/" % (ip,port) + str(h264))
                     h264_client.SetStreamerValue("auto_restart", 1)
+                    if options.dump:
+                        h264_client.SetStreamerValue("dumpfolder", options.dump)
+                    else:
+                        h264_client.SetStreamerValue("dumpfolder", "/home/recode/dump")
                     h264_client.StartStreamer()
                 else:
                     print("Stoping stream")
